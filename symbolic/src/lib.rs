b89cab148b3cb86f5822a9bfe5cb779f50156450
Exr0n <mail@exr0n.com>
2022-01-29 01:04:32 -0800

    untested RPN because \/ that error and :right_fist: :crab: -> :skull:

--- symbolic/src/lib.rs
+++ symbolic/src/lib.rs
@@ -5,6 +5,7 @@ use nanoid::nanoid;
 use num_rational::Rational32 as Ratio;
 
 use std::collections::{ HashSet, HashMap };
+use std::cell::RefCell;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct EventPossibilities {
@@ -29,44 +30,53 @@ pub enum Probability<'s> {
     AND (&'s Probability<'s>, &'s Probability<'s>),     // P(A n B)     not MUL: doesn't assume independence
     ADD (&'s Probability<'s>, &'s Probability<'s>),     // P(A) + P(B)  not OR: doesn't assume disjointness
     MUL (&'s Probability<'s>, &'s Probability<'s>),     // P(A) * P(B)  only for algebraic purposes
-    DIV (&'s Probability<'s>, &'s Probability<'s>),     // P(A) / P(B)  only for algebraic purposes
     SUB (&'s Probability<'s>, &'s Probability<'s>),     // P(A) - P(B)  only for algebraic purposes
+    DIV (&'s Probability<'s>, &'s Probability<'s>),     // P(A) / P(B)  only for algebraic purposes
 }
 
 #[derive(Debug)]
 pub struct Situation<'s> {
     events: HashSet<EventPossibilities>,
-    memo: HashMap<Probability<'s>, Option<Ratio>>,
+    memo: RefCell<HashMap<Probability<'s>, Option<Ratio>>>,
 }
 impl<'s> Situation<'s> {
-    fn generate_expresison_pair(&mut self, notation: &str,
+    fn generate_expresison_pair(&self, notation: &str,
                                 p1: Probability<'s>,
                                 p2: Probability<'s>,
                                 ) -> &Probability<'s> {
-        let mut ret: Probability;
+        //let mut ret: Probability;
         let mut idx: usize = 0;
         // stack machine to parse polish notation
         let mut stack: Vec<&Probability>;
 
         for char in notation.chars() {
-            match char {
+            stack.push(match char {
                 '1' => &p1,
                 '2' => &p2,
                 '!' => {
                     let x = stack.pop().expect("RPN empty `!`");
-                    self.memo.entry(Probability::NOT(x)).insert(None).key()
+                    self.memo.borrow_mut().entry(Probability::NOT(x)).insert(None).key()
                 },
                 c => {
+                    let x2 = stack.pop().expect("RPN fricked");
+                    let x1 = stack.pop().expect("RPN fricked");
                     match c {
-                        '|' => self.memo.entry(Probability::COND(&p1, &p2)).insert(None).key(),
+                        '|' => self.memo.borrow_mut().entry(Probability::COND(x1, x2)).insert(None).key(),
+                        'u' => self.memo.borrow_mut().entry(Probability::OR  (x1, x2)).insert(None).key(),
+                        'n' => self.memo.borrow_mut().entry(Probability::AND (x1, x2)).insert(None).key(),
+                        '+' => self.memo.borrow_mut().entry(Probability::ADD (x1, x2)).insert(None).key(),
+                        '*' => self.memo.borrow_mut().entry(Probability::MUL (x1, x2)).insert(None).key(),
+                        '-' => self.memo.borrow_mut().entry(Probability::SUB (x1, x2)).insert(None).key(),
+                        '/' => self.memo.borrow_mut().entry(Probability::DIV (x1, x2)).insert(None).key(),
                         c => panic!("Unknown RPN character {}", c)
                     }
                 }
-            }
+            });
         }
         stack.pop().expect("generate_expression_pair RPN invalid")
     }
-    fn generate_neighbors<'a>(&mut self, cur: &'a Probability<'s>) -> Vec<&Probability<'s>> {
+    fn generate_neighbors(&self, cur: &Probability<'s>) -> Vec<&Probability<'s>> {
+        // TODO: so like, this actually doesn't work because i need to :fist_right: :crab:
         match cur {
             &Probability::UNIT(ev) => /* TODO: no clue what the first & on this line does */ {
                 self.events.iter().filter_map(|alt| if ev == alt { None } else {
@@ -88,5 +98,10 @@ impl<'s> Situation<'s> {
             },
             _ => Vec::new(),
         }
+    }
+    fn search() {
+        // TODO: implement
+        // start by adding the goal to the queue
+        // repeatedly expand it, and add anything we don't know yet into the queue or smt
     }
 }
