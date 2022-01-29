76d67705f77e1f1e55e5f03c2fb1bbfe3a6b3d99
Exr0n <mail@exr0n.com>
2022-01-29 00:49:53 -0800

    okay why can't you find entry::insert

--- symbolic/src/lib.rs
+++ symbolic/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(entry_insert)]
+
 use bit_set::BitSet;
 use nanoid::nanoid;
 use num_rational::Rational32 as Ratio;
@@ -9,6 +11,11 @@ pub struct EventPossibilities {
     possibilities: BitSet,
     id: String,
 }
+impl EventPossibilities {
+    fn new(possibilities: BitSet) -> EventPossibilities {
+        EventPossibilities { id: nanoid!(), possibilities }
+    }
+}
 
 /// immutability gang
 /// has lifetime 's, which is the lifetime of the situation. every probablitiy will be owned by the
@@ -16,6 +23,7 @@ pub struct EventPossibilities {
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Probability<'s> {
     UNIT(&'s EventPossibilities),                       // P(X)
+    NOT (&'s Probability<'s>),                          // P(!X)
     COND(&'s Probability<'s>, &'s Probability<'s>),     // P(A | B)
     OR  (&'s Probability<'s>, &'s Probability<'s>),     // P(A u B)
     AND (&'s Probability<'s>, &'s Probability<'s>),     // P(A n B)     not MUL: doesn't assume independence
@@ -24,21 +32,11 @@ pub enum Probability<'s> {
     DIV (&'s Probability<'s>, &'s Probability<'s>),     // P(A) / P(B)  only for algebraic purposes
     SUB (&'s Probability<'s>, &'s Probability<'s>),     // P(A) - P(B)  only for algebraic purposes
 }
-impl<'s> Probability<'s> {
-//    fn generate_neighbors(&self) -> Vec<Probability<'s>> {
-//        match self {
-//            Self::UNIT(ev) => vec![
-//                // this can't work, because it needs to know what else exists in the Situation
-//            ],
-//            _ => Vec::new(),
-//        }
-//    }
-}
 
 #[derive(Debug)]
 pub struct Situation<'s> {
     events: HashSet<EventPossibilities>,
-    memo: HashMap<Probability<'s>, Ratio>,
+    memo: HashMap<Probability<'s>, Option<Ratio>>,
 }
 impl<'s> Situation<'s> {
     fn generate_expresison_pair(&mut self, notation: &str,
@@ -48,19 +46,32 @@ impl<'s> Situation<'s> {
         let mut ret: Probability;
         let mut idx: usize = 0;
         // stack machine to parse polish notation
-        let mut stack: Vec<Probability>;
+        let mut stack: Vec<&Probability>;
 
-        //for char in notation {
-        //    match char {
-        //        
-        //    }
-        //}
+        for char in notation.chars() {
+            match char {
+                '1' => &p1,
+                '2' => &p2,
+                '!' => {
+                    let x = stack.pop().expect("RPN empty `!`");
+                    self.memo.entry(Probability::NOT(x)).insert(None).key()
+                },
+                c => {
+                    match c {
+                        '|' => self.memo.entry(Probability::COND(&p1, &p2)).insert(None).key(),
+                        c => panic!("Unknown RPN character {}", c)
+                    }
+                }
+            }
+        }
+        stack.pop().expect("generate_expression_pair RPN invalid")
     }
-    fn generate_neighbors<'a>(&mut self, cur: &'a Probability<'s>) -> Vec<Probability<'s>> {
+    fn generate_neighbors<'a>(&mut self, cur: &'a Probability<'s>) -> Vec<&Probability<'s>> {
         match cur {
             &Probability::UNIT(ev) => /* TODO: no clue what the first & on this line does */ {
                 self.events.iter().filter_map(|alt| if ev == alt { None } else {
-                    Some(self.generate_expresison_pair("+*|122*|1!2!2",
+                    //Some(self.generate_expresison_pair("+*|122*|1!2!2",     // polish
+                    Some(self.generate_expresison_pair("12|2*12!|2!*+",     // reverse polish
                         Probability::<'s>::UNIT(ev),
                         Probability::<'s>::UNIT(alt),
                    ))
