e2fddd8f23ad80a076eae98345043a3235740670
Exr0n <mail@exr0n.com>
2022-01-28 23:34:14 -0800

    ^^^ dont expect good commit messages; ith thats all combos

--- symbolic/src/lib.rs
+++ symbolic/src/lib.rs
@@ -3,7 +3,7 @@ use nanoid::nanoid;
 
 #[derive(Debug, Hash)]
 pub struct EventPossibilities {
-    possibilities: Bitset,
+    possibilities: BitSet,
     id: String,
 }
 
@@ -12,8 +12,14 @@ pub struct EventPossibilities {
 /// situation it occupies.
 #[derive(Debug, Hash)]
 pub enum Probability<'s> {
-    UNIT(EventPossibilities),
-    COND(&'s Probability<'s>, &'s Probability<'s>),
+    UNIT(EventPossibilities),                           // P(X)
+    COND(&'s Probability<'s>, &'s Probability<'s>),     // P(A | B)
+    OR  (&'s Probability<'s>, &'s Probability<'s>),     // P(A u B)
+    AND (&'s Probability<'s>, &'s Probability<'s>),     // P(A n B)     not MUL: doesn't assume independence
+    ADD (&'s Probability<'s>, &'s Probability<'s>),     // P(A) + P(B)  not OR: doesn't assume disjointness
+    MUL (&'s Probability<'s>, &'s Probability<'s>),     // P(A) * P(B)  only for algebraic purposes
+    DIV (&'s Probability<'s>, &'s Probability<'s>),     // P(A) / P(B)  only for algebraic purposes
+    SUB (&'s Probability<'s>, &'s Probability<'s>),     // P(A) - P(B)  only for algebraic purposes
 }
 
 #[derive(Debug)]
