b0bfbfdb05c7508cacccaa6fdddc62396a918fee
Exr0n <mail@exr0n.com>
2022-03-09 09:46:18 -0800

    problem 2: standard deviation gets smaller

--- central_limit_theorem/main.jl
+++ central_limit_theorem/main.jl
@@ -1,24 +1,35 @@
 using Plots
 using Distributions;
+using Statistics;
+using ProgressMeter;
 
 num_samples = 30
 
 function sample_N(n, sample)
-    ret = 0
-    for i in 1:n
-        ret += sample()
-    end
+    ret = zeros(n)
+    ret = map(x -> sample(), ret)
 
-    return ret/n
+    mean = sum(ret) / n
+
+    return mean
 end
+
+# function get_stddev(arr)
+#     mean = sum(arr) / length(arr)
+#     return mean, stdm(arr)
+# end
 
 println("starting computation...")
 
 function plot_central_limit_thm(n_rounds, n_samples, sample_fn)
-    p = histogram(randn(1000), bins=:scott, weights=repeat(1:5, outer=200))
-    display(p)
-    println("press enter to continue")
-    readline()
+    means = [sample_N(n_samples, sample_fn) for _ = 1:n_rounds]
+    println("means len: $(length(means)), stddev: $(std(means))")
+    for p in 2:6
+        p = histogram(means[1:10^p], bins=40, title="rounds: $(10^p)")
+        display(p)
+        println("press enter to continue")
+        readline()
+    end
 end
 
-plot_central_limit_thm(10000, num_samples, rand)
+plot_central_limit_thm(1e6, num_samples, rand)
